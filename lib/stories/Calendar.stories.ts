7406e2b05505e6d1cf5373fceb42b5b428a1495b
Dikendev <diken.dev@gmail.com>
2025-03-25 18:32:32 -0300

    Update Storybook assets and configuration, including new iframe and DocsRenderer scripts

--- lib/stories/Calendar.stories.ts
+++ lib/stories/Calendar.stories.ts
@@ -10,9 +10,9 @@ const meta = {
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: "padded",
-        // toc: {
-        //     disable: true, // 👈 Disables the table of contents
-        // },
+        toc: {
+            disable: true, // 👈 Disables the table of contents
+        },
     },
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ["autodocs"],
