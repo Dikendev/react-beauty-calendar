39c63cdaafdc38aa6ad8317f58fb8c42162dfff4
Dikendev <diken.dev@gmail.com>
2025-03-25 19:34:28 -0300

    Update Storybook iframe script and modify Calendar story parameters

--- lib/stories/Calendar.stories.ts
+++ lib/stories/Calendar.stories.ts
@@ -10,12 +10,9 @@ const meta = {
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: "padded",
-        toc: {
-            disable: true, // 👈 Disables the table of contents
-        },
     },
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-    tags: ["autodocs"],
+    tags: ["!autodocs"],
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {
         viewModes: { control: "select" },
