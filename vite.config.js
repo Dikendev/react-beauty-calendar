eab2a483e422f10006f3da55cd182ca25fbdfb4d
Dikendev <diken.dev@gmail.com>
2025-03-30 22:16:57 -0300

    fix: remove unused tsconfig.app.json and eslint.config.js files; update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), dts({ include: ["lib"] })],
+    plugins: [
+        react(),
+        libInjectCss(),
+        tailwindcss(),
+        dts({
+            include: ["lib"],
+        }),
+    ],
     resolve: {
         alias: {
             "@": resolve(__dirname, "./lib"),
@@ -18,7 +26,11 @@ export default defineConfig({
             formats: ["es", "cjs"],
         },
         rollupOptions: {
-            external: ["react", "react-dom", "react/jsx-runtime"],
+            external: [
+                "react",
+                "react-dom",
+                "react/jsx-runtime",
+            ],
             output: {
                 exports: "named",
                 assetFileNames: "build/assets/[name][extname]",
